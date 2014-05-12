4d7d3d15692a00f520f8ba44ead9b7f824cb6b33
Sander Tuin <github@sandert.me>
2014-05-13 00:42:08 +0200

    rbenv deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :ssh_options, {
 set :keep_releases, 5
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.1'
+set :rbenv_ruby, '2.1.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
