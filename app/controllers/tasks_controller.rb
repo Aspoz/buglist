1b712c52485aeea08bbd484fea6fe6c2063a57fd
Sander Tuin <github@sandert.me>
2014-05-13 12:51:13 +0200

    Completed task into other box

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,9 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:edit, :update, :destroy, :toggle]
 
   def index
-    # @tasks_done = Task.where(completed: true).order(created_at: :desc)
-    # @tasks = Task.where(completed: false).order(created_at: :desc)
-    @tasks = Task.order(created_at: :desc)
+    @tasks_done = Task.where(completed: true).order(created_at: :desc)
+    @tasks = Task.where(completed: false).order(created_at: :desc)
+    # @tasks = Task.order(created_at: :desc)
     @task = Task.new
   end
 
